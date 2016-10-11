997eab80289923cb93fa39d61521822fa103246b
rxseger <rseger@gmx.co.uk>
2016-10-11 05:14:10 +0000

    Add Python script to watch and log GPIO interrupts

---
+++ watchpins.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# watchpins.py - listen for and log GPIO input changes using interrupts
+
+import RPi.GPIO as GPIO
+import sys
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+switches = map(int, sys.argv[1:])
+if len(switches) == 0:
+	sys.stderr.write("""usage: %s [physical pin(s) to watch]
+
+example: %s 24 26 22
+
+When an input changes state (rising or falling, on interrupt), this
+program will log, once per line, the pin, a space, and the new state.
+""" % (sys.argv[0], sys.argv[0]))
+	raise SystemExit
+
+GPIO.setwarnings(False)
+GPIO.setup(switches, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def handle(pin):
+	state = GPIO.input(pin)
+	print pin,state
+
+# trigger for all on both rising and falling edges
+for pin in switches:
+	GPIO.add_event_detect(pin, GPIO.BOTH, handle)
+
+while True: time.sleep(1e6)
